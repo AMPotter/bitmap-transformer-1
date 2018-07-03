de74352e50ef94e7afc49a512a20bdb1b19f4bab
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-07-03 11:31:50 -0700

    overdue commit of almost everything

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -1,38 +1,38 @@
 const assert = require('assert');
-const { readFile } = require('fs').promises;
+const fs = require('fs');
 const BitmapTransformer = require('../lib/bitmap-transformer');
-const invert = require('../lib/invert-transform');
+const invert = require('../lib/invert-transformer');
+const { promisify } = require('util');
+const unlink = promisify(require('fs').unlink);
 
 describe('bitmap file transformer', () => {
     
-    let buffer = null;
+    let tester = null;
+    const file = './test/test-bitmap.bmp';
+    const testFile = './test/tested-invert.bmp';
+
     beforeEach(() => {
-        // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
+        return unlink(testFile)
+            .catch(err => {
+                if(err.code !== 'ENOENT') throw err;
+            });
     });
 
-    // "pinning" test, or "snapshot" test
-    it('test whole transform', () => {
-        // Use the BitmapTransformer class, 
-        // passing in the buffer from the file read
-        const bitmap = new BitmapTransformer(buffer);
-
-        // Call .transform(), which will modify the buffer.
-        // With this api, you pass in a transformation function (we are testing with "invert")
-        bitmap.transform(invert);
-
-        // After above step, the buffer has been modified
-        // and is accessible via bitmap.buffer.
-
-        // Read the output file we saved earlier as the "standard" expected output file.
-        return readFile('./test/inverted-expected.bmp')
-            .then(expected => {
-                assert.deepEqual(bitmap.buffer, expected);
+    beforeEach(() => {
+        return BitmapTransformer.create(file)
+            .then(data => {
+                tester = data;
+                return tester;
             });
+    });
 
-        // If you don't have a standard file yet, or need to update or are adding new test,
-        // you can write it out by commenting above code block, and uncomment code below 
-        // that writes the file and then visually inspect the file for correctness.
+    it('test whole transform', () => {
+        return tester.transform(invert, testFile)
+            .then(() => {
+                const expected = fs.readFileSync('./test/inverted-expected.bmp');
+                const result = fs.readFileSync(testFile);
 
-        // return fs.writeFileSync('./test/inverted-expected.bmp', bitmap.buffer);
+                assert.deepEqual(expected, result);
+            });
     });
 });
